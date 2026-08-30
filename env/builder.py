f9661c6bc11499eb06615df03de5a88891a639d4
xuen012 <xuen998@gmail.com>
2026-08-31 02:29:30 +0800

    base settings to further construct upon.

---
+++ env/builder.py
@@ -0,0 +1,16 @@
+from tampanda import ArmSceneBuilder
+
+def make_builder() -> ArmSceneBuilder:
+    
+    builder = ArmSceneBuilder()
+    builder.add_resource("table",  TABLE_SYMBOLIC_TEMPLATE)
+    builder.add_resource("cube", BLOCK_SMALL_TEMPLATE)
+    builder.add_resource("block", BLOCK_MEDIUM_TEMPLATE)
+    builder.add_resource("pudding_box", {"type": "ycb", "name": "pudding_box"})
+    builder.add_resource("gelatin_box", {"type": "ycb", "name": "gelatin_box"})
+    builder.add_resource("brick", {"type": "ycb", "name": "foam_brick"})
+    builder.add_resource("wood_block", {"type": "ycb", "name": "wood_block"})
+    builder.add_object("table", name="table", pos=[0.75,  0.80, 0.00])
+
+    return builder
+        
